5b288eba7e432a121a2a7d443144eb918fa5cb9b
ParamagicDev <konnor5456@gmail.com>
2018-04-16 05:21:23 -0400

    Added game loop. Added user input & check user input. Fixed loading words array. Computer auto generates word.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -6,22 +6,63 @@
 
 # hangman game
 class Hangman
-  attr_accessor :incorrect_guesses, :possible_words
-  def intialize
-    @incorrect_guesses = 0
-    @possible_words = load_words
+  attr_reader :num_incorrect_guesses, :secret_word, :guesses
+
+  MAX_INCORRECT_GUESSES = 7
+  SAFE_WORDS = %w[quit save].freeze
+
+  def initialize
+    @guesses = []
+    @num_incorrect_guesses = 0
+    @secret_word = select_random_word
   end
 
-  def load_words
-    possible_words = []
-    all_words = File.readlines('5desk.txt') { |word| word }
-    all_words.select do |word|
-      next if word.length > 12 || word.length < 5
-      possible_words << word
+  def play_game
+    puts 'Welcome to Hangman!!'
+    loop do
+      input = user_input
+      break if input == 'quit'
+      @guesses << input
+      p @guesses
     end
 
-    possible_words
+    puts 'Goodbye!'
+  end
+
+  private
+
+  # loads words into an array, selects only words of a certain length
+  # then selects and then lowercases the word
+  def select_random_word
+    words = File.readlines('5desk.txt', chomp: true) { |word| word }
+    words.select! { |word| word.length > 5 && word.length < 12 }
+    words.sample.downcase
+  end
+
+  def display_guesses
+    @guesses.each { |guess| print "#{guess} " }
+  end
+
+  def check_input(input)
+    return true if SAFE_WORDS.include?(input)
+    return false if input.length > 1 || input.empty?
+    return false if input.match?(/[^a-z]/)
+
+    true
+  end
+
+  def user_input
+    puts 'Enter your guess:'
+    input = ''
+
+    loop do
+      input = gets.chomp.downcase
+      break if check_input(input)
+      puts 'Improper input, please enter a letter.'
+    end
+
+    input
   end
 end
 
-p Hangman.new.load_words
+puts Hangman.new.play_game
