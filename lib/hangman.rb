51f483ccb19d4e594c8c369c98a355b63431c0be
ParamagicDev <konnor5456@gmail.com>
2018-04-19 03:02:41 -0400

    Added multiple functions.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -15,21 +15,48 @@ class Hangman
     @guesses = []
     @num_incorrect_guesses = 0
     @secret_word = select_random_word
+    @display_word = Array.new(@secret_word.length, '_')
   end
 
   def play_game
-    puts 'Welcome to Hangman!!'
     loop do
+      display_information
+
       input = user_input
       break if input == 'quit'
       @guesses << input
-      p @guesses
+      check_guess(input)
     end
 
     puts 'Goodbye!'
   end
 
+  def display_information
+    system 'clear'
+    puts 'Welcome to Hangman!!'
+    puts 'Valid commands are quit and save.'
+    p display_word
+    puts 'Previous guesses:'
+    p @guesses
+  end
+
+  def display_word
+    @display_word.join(' ')
+  end
+
   private
+
+  def player_won
+    if @display_word.include?('_')
+      puts 'You have won! Congratulations!'
+      true
+    end
+  end
+
+  def game_over
+    return true if player_won
+    return true if @num_incorrect_guesses > MAX_INCORRECT_GUESSES
+  end
 
   # loads words into an array, selects only words of a certain length
   # then selects and then lowercases the word
@@ -47,6 +74,7 @@ class Hangman
     return true if SAFE_WORDS.include?(input)
     return false if input.length > 1 || input.empty?
     return false if input.match?(/[^a-z]/)
+    return false if @guesses.include?(input)
 
     true
   end
@@ -62,6 +90,23 @@ class Hangman
     end
 
     input
+  end
+
+  def check_guess(guess)
+    # return false unless @secret_word.include?(guess)
+    unless @secret_word.include?(guess)
+      @num_incorrect_guesses += 1
+      return false
+    end
+
+    letter_indexes = []
+    secret_word = @secret_word.split('')
+    secret_word.each_with_index do |item, index|
+      next unless item == guess
+      letter_indexes << index
+    end
+
+    letter_indexes.each { |index| @display_word[index] = guess }
   end
 end
 
